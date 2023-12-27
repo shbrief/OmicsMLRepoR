1d7a8dc5dd195b0721df8740a80da04274e0d0f5
Sehyun Oh <shbrief@hotmail.com>
2023-12-27 11:46:24 -0500

    ontoTraverse function

--- R/data.R
+++
@@ -1,7 +0,0 @@
-#' @name HP
-#' @title Human Phenotype Ontology index
-#' @docType data
-#' @description Human Phenotype Ontology (HPO) comprising a list of 
-#' lists/vectors of properties of HPO terms indexed by term ID
-#' @format List of lists and vectors
-NULL
--- R/ontoTraverse.R
+++ R/ontoTraverse.R
@@ -1,64 +1,47 @@
-# Load ontology
-#<<<<<<<<<<<<<<<<< Save ontology in the package or cloud repository? As obo file (in extdata) vs. Rdata (in data)
-#<<<<<<<<<<<<<<<<< Long loading time for large ontology like NCIT
-.loadOntology <- function(source) {
-    
-    if (toupper(source) %in% c("NCIT", "CHEBI")) { 
-        dir <- system.file("extdata", package = "OmicsMLRepoR")
-        ontoTable <- read.csv(file.path(dir, "ontoTable.csv"))
-        ind <- which(ontoTable$ontologyDB == toupper(source))
-        ontoDB <- file.path(dir, ontoTable$filename[ind])
-        
-        ontology <- ontologyIndex::get_ontology(ontoDB)
-        return(ontology)
-    } else if (toupper(source) == "HP") { #<<<<<<<<<< For test: update once all the ontology is provided in the same way
-        data("hpo")
-        return(hpo)
-    }
-}
-
-
-#' Extract ancestors/descendants of a term from ontology tree
+#' Extract both ancestors and descendants of a term from ontology tree
 #' 
-#' @import ontologyIndex
+#' @import rols
 #' @importFrom ontologyPlot onto_plot
 #' 
+#' @param termId Ontology term id started with the ontology prefix and id
+#' separated by colon, such as \code{NCIT:C15280}. 
 #' @param source Ontology database id. Currently available options are 
 #' \code{NCIT, CHEBI, HP}.
-#' @param termId Ontology term id started with \code{{ontology database id}:}, 
-#' such as \code{NCIT:C15280}. Accept term itself in addition to term id??
-#' @param plot Default is \code{TRUE}. If it is set to \code{FALSE}, this 
-#' function will return a character vector of associate ontology terms. 
+#' @param plot Under the default (\code{FALSE}), this function returns a 
+#' character vector of associate ontology terms. If it is set to \code{TRUE}, 
+#' it will plot the ontology tree of the returned term. (Currently, not 
+#' supported.) 
 #' 
 #' @return Ontology plot including queried term and its associated ancestors
 #' and descendants terms. With the \code{plot=FALSE} argument, it will return
 #' a character vector of associated ontology term ids.  
 #' 
 #' @examples
-#' ontoTraverse(source = "ncit", termId = "NCIT:C15280")
-#' ontoTraverse(source = "chebi", termId = "CHEBI:5262")
-#' ontoTraverse(source = "hp", termId = "HP:0003003")
+#' ontoTraverse(termId = "NCIT:C15280", source = "ncit")
+#' ontoTraverse(termId = "CHEBI:5262", source = "chebi")
+#' ontoTraverse(termId = "HP:0011793", source = "hp")
 #' 
 #' @export
-ontoTraverse <- function(source, 
-                         termId, 
-                         plot = TRUE,
+ontoTraverse <- function(termId, 
+                         source, 
+                         plot = FALSE,
                          includeDefinition = FALSE) {
     
     # Load ontology
-    ontology <- .loadOntology(source)
+    ol <- Ontologies()
+    ontology <- ol[[source]]
+    
+    trm <- term(ontology, termId)
     
     # Get the full tree of direct ancestors/descendants of `termId` input
-    all_connected_terms <- c(get_ancestors(ontology = ontology, 
-                                           terms = termId), 
-                             get_descendants(ontology = ontology, 
-                                             roots = termId, 
-                                             exclude_roots = TRUE))
+    all_connected_terms <- c(names(termId(parents(trm))), 
+                             names(termId(trm)), 
+                             names(termId(children(trm))))
     
     if (isFALSE(plot)) {
         return(all_connected_terms)
     } else {
-        # Highlight the queied termId in the tree
+        # Highlight the quried termId in the tree
         term_ind <- which(all_connected_terms == termId)
         fillcolors <- rep("powderblue", length(all_connected_terms))
         fillcolors[term_ind] <- "yellow"
@@ -69,47 +52,4 @@ ontoTraverse <- function(source,
                                             fillcolor = fillcolors)
         return(plot_all)
     }
-}
-
-
-.get_family <- function(source,
-                        termId, 
-                        property,
-                        plot) {
-    
-    ontology <- .loadOntology(source)
-    res <- get_term_property(ontology = ontology,
-                             property = property,
-                             term = termId,
-                             as_names = TRUE)
-    
-    if (isTRUE(plot)) {
-        ontologyPlot::onto_plot(ontology,
-                                terms = c(termId, names(res)),
-                                fillcolor = c("yellow", 
-                                              rep("powderblue", length(res))))
-    } else {
-        return(res)
-    }
-}
-
-#' Get the parents of a requested ontology term
-#'
-#' @examples
-#' get_parents(source = "chebi", termId = "CHEBI:5262")
-#' get_parents(source = "hp", termId ="HP:0025031")
-#' 
-get_parents <- function(source, termId, plot = TRUE) {
-    .get_family(source, termId, property = "parents", plot = plot)
-    
-}
-
-#' Get the parents of a requested ontology term
-#'
-#' @examples
-#' get_children(source = "hp", termId ="HP:0025031")
-#' 
-get_children <- function(source, termId, plot = TRUE) {
-    .get_family(source, termId, property = "children", plot = plot)
-    
 }
