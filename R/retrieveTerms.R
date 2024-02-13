6b646c73d6c44a99402ed999a8381b7363b9679a
krlong68 <krlong586@gmail.com>
2024-02-13 12:29:57 -0800

    helper functions for retrieving maps and ancestors

--- R/retrieveTerms.R
+++ R/retrieveTerms.R
@@ -1,8 +1,60 @@
 # temporary library loading
 library(rols)
+library(tidyverse) #stringr str_match
 #library(svDialogs)
 
+# Temporary variables
+data_repo_dir <- "/home/kaelyn/Desktop/Work/OmicsMLRepoData"
+
+cMD_mapdir <- paste0(data_repo_dir, "/curatedMetagenomicData/maps")
+cBioPortalData_mapdir <- paste0(data_repo_dir, "/cBioPortalData/maps")
+
 ## Functions to retrieve used terms from raw user input
+
+#' Retrieve ontology term maps from a directory with .csv files
+#' 
+#' @importFrom tidyverse str_match
+#' 
+#' @param directory Path to directory containing .csv map files
+#' @param map_file_pattern Regular expression to detect map files; May contain a capturing group to retrieve feature names from map filenames
+#' 
+#' @return List of maps as data frames
+#' 
+getMaps <- function(directory, map_file_pattern) {
+  files <- list.files(directory, pattern = map_file_pattern)
+  filepaths <- sapply(files, function (x) paste0(directory, "/", x), USE.NAMES = FALSE)
+  maps <- lapply(filepaths, read.csv)
+  features <- str_match(files, map_file_pattern)
+  if (ncol(features) > 1) {
+    features <- features[,2]
+    names(maps) <- features
+  }
+  
+  return(maps)
+}
+
+#' Retrieve and save ancestors of ontology terms
+#'
+#' @param ids Character vector of term ids
+#' @param dbs Character vector of corresponding ontology names. Single string also accepted if all terms share a single ontology.
+#' 
+#' @return List of character vectors of ancestors named by original term
+#' 
+getAncestors <- function(ids, dbs) {
+  map <- data.frame(id = ids,
+                    db = dbs)
+  tryCatch({
+    onto_frames <- split(map, map$db)
+    onto_terms <- lapply(onto_frames, function(x) x$id)
+    onto_nodes <- mapply(function(n, t) getNodes(n, t), names(onto_terms), onto_terms, SIMPLIFY = FALSE)
+    onames <- unlist(lapply(onto_nodes, function(x) names(x)), use.names = FALSE)
+    oancs <- unlist(onto_nodes, recursive = FALSE, use.names = FALSE)
+    names(oancs) <- onames
+    return(oancs)
+  }, error = function(e) {
+    print(e)
+  })
+}
 
 #' Take user input and return potential terms within used ontologies
 #' 
