43750d3d6558a42355afd33ad21d95458c482853
krlong68 <krlong586@gmail.com>
2024-02-21 10:36:28 -0800

    finished scripts, inst/script to save ancestor files, preliminary cMD ancestor file

--- R/retrieveTerms.R
+++ R/retrieveTerms.R
@@ -1,13 +1,3 @@
-# temporary library loading
-library(rols)
-library(tidyverse)
-
-# Temporary variables
-data_repo_dir <- "/home/kaelyn/Desktop/Work/OmicsMLRepoData"
-
-cMD_mapdir <- paste0(data_repo_dir, "/curatedMetagenomicData/maps")
-cBioPortalData_mapdir <- paste0(data_repo_dir, "/cBioPortalData/maps")
-
 ## Functions to retrieve used terms from raw user input
 
 #' Retrieve ontology term maps from a directory with .csv files
@@ -71,17 +61,18 @@ getAncestors <- function(ids, dbs) {
 #' 
 #' @importFrom rols OlsSearch olsSearch
 #' 
-#' @param search search string
-#' @param ontology optional character vector of ontology names
-#' @param num_results optional number of results to return; default = 20
+#' @param search Character search string
+#' @param exact Optional Boolean; Defaults to FALSE; Whether to restrict the OLS search to exact matches
+#' @param ontology Optional character vector of ontology names to restrict OLS search to
+#' @param num_results Optional number of results to return; default = 20
 #' 
 #' @return Character vector of ontology term ids
 #' 
-ontoSearch <- function(search, ontology = NA, num_results = 20) {
+ontoSearch <- function(search, exact = FALSE, ontology = NA, num_results = 20) {
   if (is.na(ontology)) {
-    qry <- OlsSearch(q = search, rows = num_results)
+    qry <- OlsSearch(q = search, exact = exact, rows = num_results)
   } else {
-    qry <- OlsSearch(q = search, ontology = ontology, rows = num_results)
+    qry <- OlsSearch(q = search, exact = exact, ontology = ontology, rows = num_results)
   }
   
   qry <- olsSearch(qry)
@@ -92,8 +83,8 @@ ontoSearch <- function(search, ontology = NA, num_results = 20) {
 
 #' Find base terms for a single attribute that are descendants of search results
 #' 
-#' @param anc_lists list of term ancestors for a single attribute
-#' @param result_ids list of ids returned by search function
+#' @param anc_lists List of term ancestors for a single attribute
+#' @param result_ids List of ids returned by search function
 #'
 #' @return Character vector of ontology term ids
 #'
@@ -104,9 +95,9 @@ findRelated <- function(anc_lists, result_ids) {
 
 #' Filter metadata table by ontology term ids in specific attribute
 #' 
-#' @param metadata metadata table
-#' @param feature column name
-#' @param ids list of ids to filter by
+#' @param metadata Metadata table
+#' @param feature String; Column name to filter by
+#' @param ids List of ids to filter by
 #' 
 #' @return Metadata table filtered by provided ontology term ids in provided attribute
 #' 
@@ -120,36 +111,61 @@ filterMetadata <- function(metadata, feature, ids) {
 
 #' Wrapper to save ancestor info for all curated features within a metadata database (cMD/cBioPortalData)
 #' 
+#' @importFrom tidyverse select all_of bind_rows separate_longer_delim distinct
+#' 
 #' @param map_directory Path to directory containing .csv map files
-#' @param map_file_pattern Regular expression to detect map files; Must contain a capturing group to retrieve feature names from map filenames
-#' @param id_column_name Name of ontology term id column; Must be the same across all maps
-#' @param target_directory Path to directory to save info to
+#' @param target_file Path to file to save info to
+#' @param map_file_pattern Regular expression to detect map files
+#' @param id_column_name Name of ontology term id column; Must be the same across all maps; Defaults to "curated_ontology_term_id"
+#' @param db_column_name Name of ontology name column; Must be the same across all maps; Defaults to "curated_ontology_term_db"
 #'  
-saveInfo <- function(map_directory, map_file_pattern, id_column_name, target_directory) {
+saveInfo <- function(map_directory, target_file, map_file_pattern,
+                     id_column_name = "curated_ontology_term_id",
+                     db_column_name = "curated_ontology_term_db") {
   # retrieve maps from directory
   all_maps <- getMaps(map_directory, map_file_pattern)
   
-  # transform maps
-  all_ids <- lapply(all_maps, function(x) unique(unlist(strsplit(x[, id_column_name], split = ";"))))
+  # transform maps; should be unnecessary after maps are fixed
+  #all_ids <- lapply(all_maps, function(x) unique(unlist(strsplit(x[, id_column_name], split = ";"))))
   
-  # get ancestors for each map
-  all_ancs <- lapply(all_maps, function())
+  # pull unique terms and dbs from all maps
+  all_terms <- lapply(all_maps, function(x) select(x, all_of(c(id_column_name, db_column_name)))) %>%
+    bind_rows() %>%
+    separate_longer_delim(all_of(c(id_column_name, db_column_name)), delim = ";") %>% # should be able to be removed, could keep in just in case
+    distinct()
+  
+  # get ancestors
+  all_ancs <- getAncestors(all_terms[, id_column_name], all_terms[, db_column_name])
+  
+  # save ancestors to file
+  comp_ancs <- lapply(all_ancs, function(x) paste(x, collapse = ";"))
+  anc_frame <- data.frame(terms = names(comp_ancs),
+                          ancestors = unname(unlist(comp_ancs)))
+  write.csv(anc_frame, target_file, row.names = FALSE)
 }
 
 #' Wrapper to easily search metadata with plain text
 #' 
-#' @param search search string
-#' @param metadata metadata table
-#' @param feature column name
+#' @param search Character search string
+#' @param metadata Metadata table
+#' @param anc_file Character string indicating ancestor file to access; Either "cMD" or "cBioPortalData"
+#' @param feature String; Column name to filter by
+#' @param exact Optional Boolean; Defaults to FALSE; Whether to restrict the OLS search to exact matches
+#' @param onto Optional character vector of ontology names to filter OLS search results; Defaults to NA
+#' @param num_results Optional number of results to accept from the OLS search; Defaults to 20
 #' 
 #' @return Metadata table filtered by provided ontology term ids in provided attribute
 #' 
-searchMetadata <- function(search, metadata, feature) {
+searchMetadata <- function(search, metadata, feature, exact = FALSE, onto = NA, num_results = 20) {
   # get ontology search results
-  anc_terms <- ontoSearch(search)
+  anc_terms <- ontoSearch(search, exact = exact, ontology = onto, num_results = num_results)
   
   # get stored info on selected feature
-  feature_info <- NA # location of .rds for feature
+  dir <- system.file("extdata", package = "OmicsMLRepoR")
+  f_name <- paste0(anc_file, "_ancestors.csv")
+  feature_info <- readLines(file.path(dir, "cMD_ancestors.csv"))
+  anc_list <- lapply(feature_info$ancestors, function(x) unlist(strsplit(x, split = ";")))
+  names(anc_list) <- feature_info$terms
   
   # get feature terms that are descendants of search results
   terms_to_find <- findRelated(feature_info, anc_terms)
@@ -157,14 +173,4 @@ searchMetadata <- function(search, metadata, feature) {
   # filter metadata table
   filtered_meta <- filterMetadata(metadata, feature, terms_to_find)
   return(filtered_meta)
-}
-
-
-#' save ancestors for features
-#'
-#' @param anc_list list of ancestors named by original term
-#' @param feature feature name
-#' 
-saveAncs <- function(anc_list, feature) {
-  
 }
