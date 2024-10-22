5d4173b6c0258cbb046ab823566b0b9d491b6b8f
Sehyun Oh <shbrief@hotmail.com>
2024-10-22 14:34:52 -0400

    reorganize demo data

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' @name sample_metadata 
+#' @title sample_metadata
+#' @description A small data table to demonstrating the data reshaping 
+#' functions in OmicsMLRepoR.
+#' @keywords data
+#' @format A data frame with 4 rows and 7 columns
+#' @author Sehyun Oh \email{shbrief@gmail.com} 
+"sample_metadata"
+
+
+#' @name mini_cmd 
+#' @title A subset of cMD metadata
+#' @description A subset of curated version of `sampleMetadata` from the 
+#' curatedMetagenomicData (cMD, ver.3.8.0) package.
+#' @keywords data
+#' @format A data frame with 200 samples and 3 columns ('curation_id',
+#' 'hla', and 'package')
+#' @author Sehyun Oh \email{shbrief@gmail.com}
+"mini_cmd"
--- data-raw/mini_cmd.R
+++ data-raw/mini_cmd.R
@@ -5,10 +5,9 @@ library(OmicsMLRepoR)
 extDir <- "~/OmicsMLRepo/OmicsMLRepoData/inst/extdata"
 cmd <- read.csv(file.path(extDir, "cMD_curated_metadata_release.csv"))
 
-cmd_sub1 <- cmd %>%
+mini_cmd <- cmd %>%
     filter(!is.na(hla)) %>%
     .[1:200,] %>%
     select(curation_id, hla, package)
 
-dir <- "~/OmicsMLRepo/OmicsMLRepoR/inst/extdata"
-readr::write_csv(cmd_sub1, file.path(dir, "mini_cmd.csv"))
+save(mini_cmd, file = "~/OmicsMLRepo/OmicsMLRepoR/data/mini_cmd.RData")
