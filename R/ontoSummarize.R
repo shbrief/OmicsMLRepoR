cd56020c0296b7933589d25208729e5af432f732
Sehyun Oh <shbrief@hotmail.com>
2025-03-22 22:03:47 -0400

    remove the hard-coded ontology

--- R/ontoSummarize.R
+++ R/ontoSummarize.R
@@ -46,7 +46,8 @@ ontoSummarize <- function(parent, descendants, ontology) {
         paste(x, collapse = ";"))))
     
     ids_to_convert <- unique(c(names(finalgroups), unlist(unname(finalgroups))))
-    extract_ids <- str_match(ids_to_convert, "NCIT:.*$")
+    extract_ids <- str_match(ids_to_convert, paste0(toupper(ontology), ":.*$"))
+    extract_ids <- extract_ids[!is.na(extract_ids)]
     termnames <- unlist(lapply(extract_ids, function(x) termLabel(Term(ontob, x))))
     names(termnames) <- extract_ids
     group_names <- str_replace_all(names(finalgroups), termnames)
