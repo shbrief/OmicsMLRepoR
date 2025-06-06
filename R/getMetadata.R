6f569ebdcac3b75d29b1d5b999a9fe73c70fc8ac
Sehyun Oh <shbrief@hotmail.com>
2025-06-06 16:08:20 -0400

    remove wrong comment

--- R/getMetadata.R
+++ R/getMetadata.R
@@ -61,7 +61,7 @@ getMetadata <- function(database = NULL,
     # request_meta <- paste0(database, "_curated_metadata_release.csv")
     # fpath <- file.path("https://storage.googleapis.com",
     #                    bucket_name, request_meta)
-    zenodo_url <- "https://zenodo.org/records/15611405/files" # version 3 on 03.22.25
+    zenodo_url <- "https://zenodo.org/records/15611405/files"
     request_meta <- paste0(database, "_curated_metadata_release.csv")
     fpath <- file.path(zenodo_url, request_meta)
 
